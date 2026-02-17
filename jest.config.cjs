ab306e09f41873c74818c4a07a7264f5e176dfc1
Christopher <christopher@Christophers-MacBook-Pro.local>
2026-02-16 23:37:17 -0500

    cleaned up demo vault build files

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
   },
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { Plugin } from 'obsidian';
-import { registerCommands } from './commands/register';
-import { createDeps } from './create-deps';
-import type { Deps } from './deps';
-import { registerEvents } from './events/register';
-import { initI18n } from './i18n';
-import { Disposer } from './lifecycle/disposer';
-import { registerSettings } from './settings/register';
-import { registerUI } from './ui/register';
-import { registerViews } from './views/register';
+import { registerCommands } from '@/commands/register';
+import { createDeps } from '@/create-deps';
+import type { Deps } from '@/deps';
+import { registerEvents } from '@/events/register';
+import { initI18n } from '@/i18n';
+import { Disposer } from '@/lifecycle/disposer';
+import { registerSettings } from '@/settings/register';
+import { registerUI } from '@/ui/register';
+import { registerViews } from '@/views/register';
 
 export default class VaultWordCloudPlugin extends Plugin {
   private readonly disposer = new Disposer();
